4d9a22992a8e5e59b7c31f42f8ff0475c8b7490f
valko <valensantarone@gmail.com>
2024-09-10 22:58:02 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# WhoScraped
+
+Library for scrape football data from WhoScored
+
+## Instalation
+
+For instaling this library, just do:
+
+```bash
+pip install whoscraped
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="whoscraped",
+    version="0.1.0",
+    author="Valko",
+    author_email="valensantarone@gmail.com",
+    description="Scrape football data from WhoScored",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://www.github.com/valensantarone/whoscraped",
+    packages=find_packages(),
+    install_requires=[
+        "selenium"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
