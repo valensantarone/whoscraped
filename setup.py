acc826402e262bd0b128ea07824907582f39c20f
valentino <valensantarone@gmail.com>
2024-09-13 12:12:34 -0300

    Updating to v0.2.0-alpha

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="whoscraped",
-    version="0.1.0",
+    version="0.2.0",
     author="Valko",
     author_email="valensantarone@gmail.com",
     description="Scrape football data from WhoScored",
